67d4a8b8441115ff22a28dbcc803d5319795b7f2
Duda <maria.squeruque@aluno.senai.br>
2025-04-04 11:14:55 -0300

    funções2

--- main.go
+++ main.go
@@ -18,9 +18,27 @@ func main() {
 	fmt.Println(ages)
 	index := sort.SearchInts(ages, 50)
 	fmt.Println(index)
-	names := []string("bangchan", "Han", "Diego")
+	names := []string("Bangchan", "Han", "Diego")
 	sort.Strings(names)
 	fmt.Println(names)
 	fmt.Println(sort.SearchStrings(names, "Bangchan"))
+	x := 0
+	for x < 5 {
+		fmt.Println(x)
+		x++
+	}
+	for i := 0; i < 5; i++ {
+		fmt.Println("for 2: ", i)
+	}
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[1])
+	}
 
+	for index, value := range names {
+		fmt.Println("O indice é", index, "e o valor", value)
+	}
+	for index, value := range ages {
+		fmt.Println("O indice é", index, "e o valor", value)
+
+	}
 }
