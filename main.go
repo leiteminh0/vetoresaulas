827d5d34982cfb4af31aec10243120938121b67a
Duda <maria.squeruque@aluno.senai.br>
2025-04-10 10:10:57 -0300

    idade

--- main.go
+++ main.go
@@ -1,32 +1,18 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	age := 45
-	fmt.Println(age <= 50)
-	fmt.Println(age >= 50)
-	fmt.Println(age == 50)
-	fmt.Println(age != 50)
+	var idade int
 
-	if age < 30 {
-		fmt.Println("menor que 30 anos")
-	} else if age < 40 {
-		fmt.Println("menor que 40 anos")
+	fmt.Print("Quantos aninhos você tem, diva?  ")
+	fmt.Scan(&idade)
+
+	if idade < 18 {
+		fmt.Println("Você é menor de idade ")
+	} else if idade <= 60 {
+		fmt.Println("Você é adulto ")
 	} else {
-		fmt.Println("nao é menor que 40 anos")
+		fmt.Println("Você é idoso ")
 	}
-	names := []string{"bangchan", "han", "felix", "minho", "seugmin", "changbin"}
-
-	for index == 1 {
-		fmt.Println("continue apos a posiçao", index, "e valor", value)
-		continue
-	}
-	if index > 2 {
-		fmt.Println("sair apos", index)
-		break
-	}
-	fmt.Println("valor: ", value)
 }
