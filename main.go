9f8799aa90284ba6763af208e1ffb3632cc4aae6
Duda <maria.squeruque@aluno.senai.br>
2025-04-11 14:40:35 -0300

    saldo e sacar

--- main.go
+++ main.go
@@ -4,16 +4,31 @@ import (
 	"fmt"
 )
 
-func sayGreeting(nome string) {
-	fmt.Println("Ola!", "Bangchan")
+var saldo int
+
+func getsaldo() {
+	fmt.Println("digite o quanto quer depositar")
+	var deposito int
+	fmt.Scan(&deposito)
+	saldo = saldo + deposito
+	fmt.Println("seu saldo atual é", saldo)
 }
 
-func addNumber(numero1 int, numero2 int) int {
-	return numero1 + numero2
+func getsacar() {
+	var sacar int
+	fmt.Println("digite oque deseja sacar")
+	fmt.Scan(&sacar)
+	saldo = saldo - sacar
 
 }
+
 func main() {
-	sayGreeting("Bangchan")
-	resultado := addNumber(10, 20)
-	fmt.Println(resultado)
+	fmt.Println("digite 1 para depositar, e 2 para sacar")
+	var opcao int
+	fmt.Scan(&opcao)
+	if opcao == 1 {
+		getsaldo()
+	} else {
+		getsacar()
+	}
 }
